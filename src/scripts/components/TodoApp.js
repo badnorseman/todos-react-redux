a4409ec415f3fe05483192d502591655988e37e0
Dan Jensen <dj@fitbird.com>
2015-12-20 20:26:27 -0500

    divide modules into components and containers

--- src/scripts/components/TodoApp.js
+++ src/scripts/components/TodoApp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AddTodo from "../containers/AddTodo";
+import AddTodo from "./AddTodo";
 import Footer from "./Footer";
 import VisibleTodoList from "../containers/VisibleTodoList";
 
